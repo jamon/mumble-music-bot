fbf7c4461af2fcaa7c3b95a7f9f12c496b5a9e45
Jamon Terrell <git@jamonterrell.com>
2015-05-17 21:47:08 -0500

    updates, radio, etc

--- bot.js
+++ bot.js
@@ -63,9 +63,75 @@ var locked = false;
 var playStream, decoderStream, mumbleStream;
 var sessions = {};
 var results = [];
-var volume = 0.1;
+var volume = 0.06;
+var queue = [];
+var playing = false;
 var client = mumble.connect('mumble://a1ive.org', options, function(error, connection) {
     if(error) { throw new Error(error); }
+
+
+    var checkQueue = function() {
+        if(playing || queue.length === 0) return;
+        var next = queue.shift();
+        play(next.track, next.userName);
+    };
+    var play = function(track, userName) {
+        getImage(track.albumArtRef[0].url, function(err, data) {
+            if(err) console.error("error loading album art", err);
+            console.log("loaded album art...");
+            var img = "<img style='float: left;' src=\"data:image/jpg;base64," + data + "\" />";
+            var message = [
+                img,
+                "<div style='float: left; padding-left: 15px; margin-top: -50px; background: #fff;'>",
+                    "<h3 style='margin: 0;'>", track.artist, "</h3>",
+                    "<p style='margin-top: 5px;'>", track.title, "</p>",
+                    "<p style='font-style: italic; margin: 0;'>", " (requested by ", userName, ")", "</p>",
+                "</div>",
+                track.primaryVideo ? "<div><a href='http://www.youtube.com/watch?v=" + track.primaryVideo.id + "'>youtube</a></div>" : ""
+            ].map(function(e) {
+                return typeof e === "undefined" ? "" : e.replace("&", "&amp;", "g");
+            }).join("\n");
+            //console.log("message:", message);
+            connection.user.channel.sendMessage(message);
+        });
+        pm.getStreamUrl(track.storeId, function(err, streamUrl) {
+            if(err) {
+                console.error(err);
+                respond("Error: " + err.toString());
+                return;
+            }
+
+            var req = https.request(url.parse(streamUrl), function(streamRes) {
+                playStream = streamRes;
+                decoderStream = lame.Decoder();
+                playStream.pipe(decoderStream);
+                decoderStream.on('format', function(format) {
+                    if(typeof mumbleStream !== "undefined" && typeof mumbleStream.close === "function") mumbleStream.close();
+                    mumbleStream = connection.inputStream({channels: format.channels, sampleRate: format.sampleRate, gain: volume})
+                    decoderStream.pipe(mumbleStream);
+                    mumbleStream.on('finish', function() {
+                        console.log("#####################, finish!!!!");
+                        playing = false;
+                        checkQueue();
+                    });
+                });
+            });
+            req.end();
+        });
+    };
+    var playTrack = function(track, userName, next) {
+        if(!playing) {
+            playing = true;
+            play(track, userName);
+        } else {
+            var queueItem = {track: track, userName: userName};
+            if(next) {
+                queue.unshift(queueItem);
+            } else {
+                queue.push(queueItem);
+            }
+        }
+    };
 
     connection.authenticate('MusicBot');
     connection.on('initialized', function() {
@@ -88,6 +154,12 @@ var client = mumble.connect('mumble://a1ive.org', options, function(error, conne
         console.log(userName + ":", event.message);
         if(event.message.match(/^stop|st[a]+[h]+p|eat shit/i)) {
             if(typeof mumbleStream !== "undefined" && typeof mumbleStream.close === "function") mumbleStream.close();
+            playing = false;
+            checkQueue();
+        } else if(event.message.match(/^clearqueue/i)) {
+            if(typeof mumbleStream !== "undefined" && typeof mumbleStream.close === "function") mumbleStream.close();
+            playing = false;
+            queue = [];
         } else if(event.message.match(/^lock/i)) {
             locked = true;
         } else if(event.message.match(/^unlock/i)) {
@@ -116,54 +188,77 @@ var client = mumble.connect('mumble://a1ive.org', options, function(error, conne
 
                 results = res.entries.filter(function(r) { return r.type === '1'; });
                 var tracks = results.map(function(t, i) {
-                    var m = [i, t.track.artist, t.track.title].join(" - ");
+                    var d = t.track.durationMillis;
+                    var duration = "";
+                    var m = Math.floor(d / 60000);
+                    if(m > 0) duration += m + "m";
+                    var s = Math.floor((d % 60000)/1000);
+                    if(s > 0) duration += s + "s";
+                    var m = [i, t.track.artist, t.track.title, duration].join(" - ");
                     return m;
                 });
                 respond("Tracks Found: <br>" + tracks.join("<br>"));
                 console.log(util.inspect(tracks, {depth: null, colors: true}));
             });
+        } else if(event.message.match(/^queue/)) {
+            var num = parseInt(event.message.substr(5));
+
+            var message = queue.map(function(q) {
+                return "<div>" + q.track.artist + " - " + q.track.title + " (" + q.userName + ")</div>".replace("&", "&amp;", "g");
+            });
+            connection.user.channel.sendMessage(message.join("\n"));
+
+        } else if(event.message.match(/^radio /i)) {
+            var num = parseInt(event.message.substr(5));
+            console.log("attempting to start radio on#", num, util.inspect(results[num], {color: true, depth: 10}));
+            if(!results[num]) return;
+            var track = results[num].track;
+
+            pm.createStation("radio123", track.storeId, "track", function(err, data) {
+                if(err) return console.error(err);
+                pm.getStationTracks(data.mutate_response[0].id, 10, function(err, resp) {
+                    if(err) return console.error(err);
+                    var tracks = resp.data.stations[0].tracks;
+                    tracks.forEach(function(t) {
+                        playTrack(t, userName);
+                    });
+                    var message = tracks.map(function(t) {
+                        return "<div>" + t.artist + " - " + t.title + "</div>".replace("&", "&amp;", "g");
+                    });
+                    message.unshift("<div>Station based on " + track.artist + " - " + track.title + "</div>");
+                    message.push("<span style='font-style: italic;'>&nbsp;&nbsp;&nbsp;requested by" + userName + "</span>");
+                    connection.user.channel.sendMessage(message.join("\n"));
+                });
+            });
+
+        } else if(event.message.match(/^playnext /i)) {
+            var num = parseInt(event.message.substr(9));
+            console.log("attempting to play #", num, util.inspect(results[num], {color: true, depth: 10}));
+            if(!results[num]) return;
+            var track = results[num].track;
+
+            var message = [
+                "<span>queued (next): ", track.artist, " - ", track.title, "</span>",
+                "<span>", " (requested by ", userName, ")", "</span>"
+            ].map(function(e) {
+                return typeof e === "undefined" ? "" : e.replace("&", "&amp;", "g");
+            }).join("\n");
+            connection.user.channel.sendMessage(message);
+            playTrack(track, userName, true);
         } else if(event.message.match(/^play /i)) {
             var num = parseInt(event.message.substr(5));
             console.log("attempting to play #", num, util.inspect(results[num], {color: true, depth: 10}));
             if(!results[num]) return;
             var track = results[num].track;
-            getImage(track.albumArtRef[0].url, function(err, data) {
-                if(err) console.error("error loading album art", err);
-                console.log("loaded album art...");
-                var img = "<img style='float: left;' src=\"data:image/jpg;base64," + data + "\" />";
-                var message = [
-                    img,
-                    "<div style='float: left; padding-left: 15px; margin-top: -50px; background: #fff;'>",
-                        "<h3 style='margin: 0;'>", track.artist, "</h3>",
-                        "<p style='margin-top: 5px;'>", track.title, "</p>",
-                        "<p style='font-style: italic; margin: 0;'>", " (requested by ", userName, ")", "</p>",
-                    "</div>",
-                    track.primaryVideo ? "<div><a href='http://www.youtube.com/watch?v=" + track.primaryVideo.id + "'>youtube</a></div>" : ""
-                ].map(function(e) {
-                    return typeof e === "undefined" ? "" : e.replace("&", "&amp;", "g");
-                }).join("\n");
-                //console.log("message:", message);
-                connection.user.channel.sendMessage(message);
-            });
-            pm.getStreamUrl(track.storeId, function(err, streamUrl) {
-                if(err) {
-                    console.error(err);
-                    respond("Error: " + err.toString());
-                    return;
-                }
 
-                var req = https.request(url.parse(streamUrl), function(streamRes) {
-                    playStream = streamRes;
-                    decoderStream = lame.Decoder();
-                    playStream.pipe(decoderStream);
-                    decoderStream.on('format', function(format) {
-                        if(typeof mumbleStream !== "undefined" && typeof mumbleStream.close === "function") mumbleStream.close();
-                        mumbleStream = connection.inputStream({channels: format.channels, sampleRate: format.sampleRate, gain: volume})
-                        decoderStream.pipe(mumbleStream);
-                    });
-                });
-                req.end();
-            });
+            var message = [
+                "<span>queued: ", track.artist, " - ", track.title, "</span>",
+                "<span>", " (requested by ", userName, ")", "</span>"
+            ].map(function(e) {
+                return typeof e === "undefined" ? "" : e.replace("&", "&amp;", "g");
+            }).join("\n");
+            connection.user.channel.sendMessage(message);
+            playTrack(track, userName);
         }
     });
     connection.on('userState', function(state) {
